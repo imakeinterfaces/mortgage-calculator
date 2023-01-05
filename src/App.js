1b0856fe3bc9e751d57f5aba0be3468211c21cee
Sam Scherer <sam.scherer@thedailybeast.com>
2023-01-04 21:06:13 -0500

    Update packages

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ function getRemainingPrincipal(mtg) {
   const principalPayment = monthlyPayment - monthlyInterest;
 
   const getRemainingPrincipal = loanAmount - principalPayment;
-  console.log("das monthly payment", principalPayment);
   return getRemainingPrincipal;
 }
 
@@ -63,7 +62,6 @@ function getEliminatedPrepaymentMonths(paymentsArray) {
 function addPrePayments(prePayments) {
   let sum = 0;
   prePayments.forEach((payment) => {
-    console.log("das payment 331", payment);
     sum = sum + payment.amount;
   });
 
@@ -154,7 +152,7 @@ function loopAmortization(
     ...mtgObj,
     loanAmount: newLoanAmount,
     currentMonthIndex,
-    paymentsArray,
+    paymentsArray: paymentsArray,
   };
 }
 
@@ -179,42 +177,25 @@ export const prePayments = [
   },
 ];
 
-export const data = [
-  [
-    "Day",
-    "Guardians of the Galaxy",
-    "The Avengers",
-    "Transformers: Age of Extinction",
-  ],
-  [1, 37.8, 80.8, 41.8],
-  [2, 30.9, 69.5, 32.4],
-  [3, 25.4, 57, 25.7],
-  [4, 11.7, 18.8, 10.5],
-  [5, 11.9, 17.6, 10.4],
-  [6, 8.8, 13.6, 7.7],
-  [7, 7.6, 12.3, 9.6],
-  [8, 12.3, 29.2, 10.6],
-  [9, 16.9, 42.9, 14.8],
-  [10, 12.8, 30.9, 11.6],
-  [11, 5.3, 7.9, 4.7],
-  [12, 6.6, 8.4, 5.2],
-  [13, 4.8, 6.3, 3.6],
-  [14, 4.2, 6.2, 3.4],
-];
-
 export const options = {
   chart: {
-    title: "Box Office Earnings in First Two Weeks of Opening",
-    subtitle: "in millions of dollars (USD)",
+    title: "Remaining Loan Balances",
+    subtitle: "Dollars (USD)",
   },
 };
 
-
-
-
-
 const { paymentsArray } = loopAmortization(mtg, mtg.startDate, prePayments);
-console.log(paymentsArray);
+
+const remap = paymentsArray
+.filter((e) => e.newLoanAmount > 0)
+.reverse()
+.map((e, i) => {
+  return [i + 1, e.newLoanAmount];
+})
+
+remap.unshift(["Month", "Mortgage 1"]);
+
+debugger;
 function App() {
   return (
     <div className="App">
@@ -261,12 +242,12 @@ function App() {
         </p>
 
         <Chart
-      chartType="Line"
-      width="100%"
-      height="400px"
-      data={data}
-      options={options}
-    />
+          chartType="Line"
+          width="100%"
+          height="400px"
+          data={remap}
+          options={options}
+        />
 
         <p>
           TOTAL MONTHS SAVED
@@ -295,7 +276,7 @@ function App() {
             </TableRow>
           </TableHeader>
 
-          {paymentsArray.reverse().map(({ newLoanAmount, nextDate }) => (
+          {paymentsArray.map(({ newLoanAmount, nextDate }) => (
             <TableBody>
               <TableRow>
                 <TableCell scope="row">
